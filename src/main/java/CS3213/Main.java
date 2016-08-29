8ec2a639012cfec075e24984b880a9ae7100dc83
miabw <miabw@users.noreply.github.com>
2016-08-29 16:35:37 +0800

    Update Main.java

--- src/main/java/CS3213/Main.java
+++ src/main/java/CS3213/Main.java
@@ -1,6 +1,9 @@
 package CS3213;
 
+
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 /**
@@ -16,6 +19,7 @@ public class Main {
 
         List<String> inputs = new ArrayList<String>();
         String userInput = sc.nextLine();
+        
         while (!userInput.isEmpty()) {
             inputs.add(userInput);
             userInput = sc.nextLine();
@@ -28,6 +32,15 @@ public class Main {
             wordsToIgnore.addWordToIgnore(inputWordToIgnore);
             inputWordToIgnore = sc.nextLine();
         }
+        
+        System.out.println("Enter required words list (terminate input by entering empty line) ");
+        String inputReqWords = sc.nextLine();
+        List<String> reqWordsList = new ArrayList<String>();
+        while (!inputReqWords.isEmpty()) {
+        	reqWordsList.add(inputReqWords);
+        	inputReqWords = sc.nextLine();
+        }
+        
 
         Alphabetizer alphabetizer = new Alphabetizer();
         for (String str : inputs) {
@@ -36,9 +49,17 @@ public class Main {
         }
 
         String[] result = alphabetizer.getSortedLines();
+        
+        KeyFilter keyfilter = new KeyFilter();
+        keyfilter.setRequiredWords(reqWordsList);
+        keyfilter.setKwicOriList(Arrays.asList(result));
+        
+        List<String> filtered_results = keyfilter.filterList();
+        
         StringBuilder builder = new StringBuilder();
+        
         String separator = System.lineSeparator();
-        for (String str : result) {
+        for (String str : filtered_results) {
             builder.append(str).append(separator);
         }
         System.out.print(builder.toString());
